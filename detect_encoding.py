0e28761cbcf3a090ef9718a5a0e51b9dcfa0cd65
lignah <02_pastime.peelers@icloud.com>
2025-06-20 20:07:20 +0900

    ​

--- detect_encoding.py
+++ detect_encoding.py
@@ -1,22 +1,22 @@
 import chardet
 
-file_name = "notice.txt"
-with open(file_name, "rb") as f:
-    rawdata = f.read()
+file_name = "input.txt"
 
-print(
-    chardet.detect(rawdata)
-)
+# with open(file_name, "rb") as f:
+#     rawdata = f.read()
+
+# print(
+#     chardet.detect(rawdata)
+# )
 
 # {'encoding': 'ascii', 'confidence': 1.0, 'language': ''}
 
 
-""" change encoding
-change_filename = "<filename>"
-with open(file_name, "r", encoding="ascii") as f:
+"change encoding"
+change_filename = "output.txt"
+with open(file_name, "r", encoding="EUC-KR") as f:
     content = f.read()
 # utf-8, utf-8-sig, euc-kr, ascii, latin1
 
 with open(change_filename, "w", encoding="utf-8") as f:
     f.write(content)
-"""
